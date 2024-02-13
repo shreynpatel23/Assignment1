c3a8f0536ce5ac8933e2255ca609f201086b23db
Shrey Patel <shreynpatel23@gmail.com>
2024-02-13 17:52:48 -0500

    update relative path for index page

--- common/header.php
+++ common/header.php
@@ -4,7 +4,7 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <a class="navbar-brand" href="index.php">Amazon</a>
+            <a class="navbar-brand" href="../index.php">Amazon</a>
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                     <a class="nav-link" href="../Book/bookList.php">Books</a>
--- index.php
+++ index.php
@@ -13,24 +13,7 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-                <a class="navbar-brand" href="index.php">Amazon</a>
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="bookList.php">Books</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="authorList.php">Authors</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <?php include './common/header.php' ?>
     <h1 class="container my-5">Hello, world!</h1>
     <footer class="my-3 text-center">
     <p class="mb-0 text-dark fw-bold">© Copyright Amazon 2024 | All rights reserved</p>
