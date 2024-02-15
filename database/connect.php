3c72452dbe38284d20477475b80a314db15e26b7
Shrey Patel <shreynpatel23@gmail.com>
2024-02-15 15:57:03 -0500

    add infinity free cred

--- database/connect.php
+++ database/connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$db_host = 'localhost';
-$db_user = 'root';
-$db_password = 'root';
-$db_db = 'Online_bookstore';
+$db_host = 'sql311.infinityfree.com';
+$db_user = 'if0_35758298';
+$db_password = 'kashiben97';
+$db_db = 'if0_35758298_Online_bookstore';
 $connect = mysqli_connect($db_host, $db_user, $db_password, $db_db);
 // Check connection
 if (mysqli_connect_errno()) {
